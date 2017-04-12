5225b660849b28fa2d90793552ac69c73f3369c8
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-12 14:58:33 +0200

    Cors finally working!

--- src/main/java/dk/surfstation/easyedit/config/SecurityConfiguration.java
+++ src/main/java/dk/surfstation/easyedit/config/SecurityConfiguration.java
@@ -3,17 +3,30 @@ package dk.surfstation.easyedit.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+// Inspiration: http://disq.us/p/13yhboy
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+				.ignoring()
+				.antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.requestMatchers()
 				.antMatchers(HttpMethod.GET, "/api/posts/filter")
+				.antMatchers(HttpMethod.GET, "/my-project/**")
 				.and()
 				.authorizeRequests()
-				.anyRequest().authenticated();
+				.antMatchers(HttpMethod.GET, "/api/posts/filter").permitAll()
+				.antMatchers(HttpMethod.GET, "/my-project/**").permitAll()
+				.anyRequest()
+				.authenticated();
 	}
 }
