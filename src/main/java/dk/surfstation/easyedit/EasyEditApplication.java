bb126d09362122bee4d54eabd34a18c91c4eeb3c
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-13 21:27:15 +0200

    Added a bit of somewhat messy db initialization.

--- src/main/java/dk/surfstation/easyedit/EasyEditApplication.java
+++ src/main/java/dk/surfstation/easyedit/EasyEditApplication.java
@@ -1,13 +1,47 @@
 package dk.surfstation.easyedit;
 
+import dk.surfstation.easyedit.domain.Post;
+import dk.surfstation.easyedit.service.PostServiceInterface;
+import dk.surfstation.easyedit.service.UserServiceInterface;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class EasyEditApplication {
+	@Bean
+	public CommandLineRunner init(UserServiceInterface userService, PostServiceInterface postService) {
+		return args -> {
+			if (!userService.findByUsername("tons").isPresent()) {
+
+				Stream.of("tons,skummet", "snot,skummet")
+						.map(s -> s.split(","))
+						.forEach(s -> userService.save(s[0], s[1]));
+
+				initPosts(userService, postService, "tons", new String[]{"tons title,content", "tons other title,other content", "tons yet another title,yet some more content"});
+				initPosts(userService, postService, "snot", new String[]{"title,content", "other title,other content", "yet another title,yet some more content"});
+				}
+		};
+	}
+
+	private void initPosts(UserServiceInterface userService, PostServiceInterface postService, String username, String[] strings) {
+		Stream.of(strings)
+				.map(s -> s.split(","))
+				.map(s -> {
+					Post post = new Post();
+					post.setTitle(s[0]);
+					post.setContent(s[1]);
+					post.setUser(userService.findByUsername(username).get());
+					return post;
+				})
+				.forEach(postService::save);
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
