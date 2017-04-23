8f027cd135e55cb852f99e0013dbb2dba39ebf7f
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-23 12:42:44 +0200

    Latest post is now selected using a native query rather than a hql query using a subselect. I'm not sure which is better... But moving on!

--- src/main/java/dk/surfstation/easyedit/repository/PostRepository.java
+++ src/main/java/dk/surfstation/easyedit/repository/PostRepository.java
@@ -8,9 +8,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
-	@Query("SELECT p FROM Post p INNER JOIN p.user WHERE p.user.username = :username ORDER BY p.updated DESC")
+	@Query("SELECT p FROM Post p WHERE p.user.username = :username ORDER BY p.updated DESC")
 	Iterable<Post> findAllByUsername(@Param("username") String username);
 
-	@Query("SELECT p FROM Post p INNER JOIN p.user WHERE p.user.username = :username AND p.updated = (SELECT MAX(p.created) FROM Post p)")
+// HQL
+//	@Query("SELECT p FROM Post p WHERE p.user.username = :username AND p.updated = (SELECT MAX(p.updated) FROM Post p WHERE p.user.username = :username)")
+// Native SQL
+	@Query(value = "SELECT p.id, p.title, p.content, p.created, p.updated, p.user_id\n" +
+			"FROM post p\n" +
+			"  INNER JOIN users u\n" +
+			"    ON p.user_id = u.id\n" +
+			"WHERE u.username = :username\n" +
+			"ORDER BY p.updated DESC\n" +
+			"LIMIT 1", nativeQuery = true)
 	Post findLatestByUsername(@Param("username") String username);
 }
