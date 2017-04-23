7b197c2501bd31cfdf9545f4ad898b4b07ffcd42
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-23 14:54:04 +0200

    Let UserService implement UserDetailsService rather than extending UserServiceInterface.

--- src/main/java/dk/surfstation/easyedit/service/UserService.java
+++ src/main/java/dk/surfstation/easyedit/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService implements UserServiceInterface {
+public class UserService implements UserServiceInterface, UserDetailsService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 
--- src/main/java/dk/surfstation/easyedit/service/UserServiceInterface.java
+++ src/main/java/dk/surfstation/easyedit/service/UserServiceInterface.java
@@ -1,11 +1,10 @@
 package dk.surfstation.easyedit.service;
 
 import dk.surfstation.easyedit.domain.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Optional;
 
-public interface UserServiceInterface extends UserDetailsService {
+public interface UserServiceInterface {
 	User save(String username, String password);
 	Optional<User> findByUsername(String username);
 	void delete(long id);
