48e08e45edd9f78d48cdcb36e7414a48eaf451eb
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-25 19:09:42 +0200

    Added some test users.

--- src/main/java/dk/surfstation/easyedit/EasyEditApplication.java
+++ src/main/java/dk/surfstation/easyedit/EasyEditApplication.java
@@ -1,43 +1,13 @@
 package dk.surfstation.easyedit;
 
-import dk.surfstation.easyedit.domain.User;
-import dk.surfstation.easyedit.service.PostServiceInterface;
-import dk.surfstation.easyedit.service.UserServiceInterface;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.stream.Stream;
-
 @SpringBootApplication
 public class EasyEditApplication {
-	@Bean
-	public CommandLineRunner init(UserServiceInterface userService, PostServiceInterface postService) {
-		return args -> {
-			if (!userService.findByUsername("tons").isPresent()) {
-
-				Stream.of("tons,skummet", "snot,skummet")
-						.map(s -> s.split(","))
-						.forEach(s -> userService.save(s[0], s[1]));
-
-				initPosts(userService, postService, "tons", new String[]{"tons title,content", "tons other title,other content", "tons yet another title,yet some more content"});
-				initPosts(userService, postService, "snot", new String[]{"title,content", "other title,other content", "yet another title,yet some more content"});
-				}
-		};
-	}
-
-	private void initPosts(UserServiceInterface userService, PostServiceInterface postService, String username, String[] strings) {
-		Stream.of(strings)
-				.map(s -> s.split(","))
-				.forEach(s -> {
-					User user = userService.findByUsername(username).get();
-					postService.save(s[0], s[1], user);
-				});
-	}
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
