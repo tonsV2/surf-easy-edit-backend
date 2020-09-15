d7a1ed17c5b6313f967391178b7b3b73161010c1
sj <sebastianthegreatful@gmail.com>
2020-09-15 14:20:02 +0700

    Formatting

--- src/main/java/dk/surfstation/easyedit/domain/User.java
+++ src/main/java/dk/surfstation/easyedit/domain/User.java
@@ -24,10 +24,8 @@ public class User {
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "users_roles",
-			joinColumns = @JoinColumn(
-					name = "user_id", referencedColumnName = "id"),
-			inverseJoinColumns = @JoinColumn(
-					name = "role_id", referencedColumnName = "id"))
+			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
 	private Collection<Role> roles;
 
 	@JsonIgnore
